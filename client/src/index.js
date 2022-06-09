6510a8f673038555e7ee931d418f61a09bf63fb5
Daniel Quintanilla <dquintanilla@socialsolutions.com>
2022-06-09 12:00:01 -0500

    HACKATHON: added ssg component library

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,7 @@
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root")
-);
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.render(  <BrowserRouter>
+  <App />
+</BrowserRouter>, document.getElementById('root'));

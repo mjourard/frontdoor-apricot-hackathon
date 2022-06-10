64f0c8329ed165370c872650b3716064d561e5a8
Matt Jourard <matt.jourard@athenasoftware.net>
2022-06-10 12:53:15 -0400

    Initial commit with the card styling of the Org view

--- client/src/components/Registration/components/OrgView/OrgView.jsx
+++ client/src/components/Registration/components/OrgView/OrgView.jsx
@@ -1,5 +1,33 @@
 import React from 'react';
+import {
+    Card,
+    ThemeProvider,
+    Button,
+    Spacer,
+    Text,
+  } from '@social-solutions/component-library';
+import styles from './OrgView.style';
 
-const OrgView = () => (<div>OrgView</div>);
+const OrgView = ({
+    legalName,
+    taxSource,
+}) => {
+
+    return (
+        <ThemeProvider>
+            <styles.ViewContainer>
+            <styles.CardContainer>
+                <styles.CardTitle>
+                    Organization Details
+                </styles.CardTitle>
+                <styles.CardContent>
+                    <Text label="Legal Name" content={legalName} />
+                    <Text label="Tax Source" content={taxSource} />
+                </styles.CardContent>
+            </styles.CardContainer>
+            </styles.ViewContainer>
+        </ThemeProvider>
+    );
+};
 
 export default OrgView;
--- client/src/components/Registration/components/OrgView/OrgView.style.jsx
+++ client/src/components/Registration/components/OrgView/OrgView.style.jsx
@@ -1,1 +1,38 @@
 import styled from 'styled-components';
+const ViewContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+    padding: 2rem;
+`;
+
+const CardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+`;
+
+const CardTitle = styled.div`
+    background-color: #20907f;
+    font-size: 22px;
+    padding: 1em;
+    color: #fafafa;
+`;
+
+const CardContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: stretch;
+    justify-content: flex-start;
+    background-color: #ffffff;
+    padding: 2rem;
+`;
+
+export default {
+    CardContainer,
+    CardContent,
+    CardTitle,
+    ViewContainer
+};

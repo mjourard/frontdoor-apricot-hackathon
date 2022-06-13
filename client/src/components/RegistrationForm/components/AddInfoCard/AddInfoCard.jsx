fa2157c7ac371d94a37e904240a1a6183693e465
Matt Jourard <matt.jourard@athenasoftware.net>
2022-06-13 18:57:19 -0400

    madethe access code and comments form editable via React stuff

--- client/src/components/RegistrationForm/components/AddInfoCard/AddInfoCard.jsx
+++ client/src/components/RegistrationForm/components/AddInfoCard/AddInfoCard.jsx
@@ -1,54 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
     Card,
     ThemeProvider,
     TextInput,
     Headline,
     SubHeading,
-  } from '@social-solutions/component-library';
-  import styles from './AddInfoCard.style';
+} from '@social-solutions/component-library';
+import styles from './AddInfoCard.style';
 
-const AddInfoCard = () => (
-    <styles.Container>
-        <ThemeProvider>
-            <Card titleHidden>
-                <styles.HeaderDiv>
-                    <styles.HeadingDiv>
-                        <Headline fontSize={22} weight={'bold'}>
-                            <styles.Coloring>
-                                Preferred Organization Information
-                            </styles.Coloring>
-                        </Headline>
-                    </styles.HeadingDiv>
-                    <styles.HeadingDiv>
-                        <SubHeading fontSize={18} weight={'normal'}>
-                            Tell us how you would prefer your organization to be listed in our system. We will use information from the Internal Revenue Service by default. If your organization’s name and/or address is different from what is listed with the IRS, please also provide a brief explanation as to why in the note field below.
-                        </SubHeading>
-                    </styles.HeadingDiv>
-                </styles.HeaderDiv>
-                <styles.TextFieldsDiv>
-                    <styles.InputDiv>
-                        <styles.InputLabel>
-                            Your Current Title/Role at this Organization
-                        </styles.InputLabel>
-                        <TextInput value={'Admin'} />
-                    </styles.InputDiv>
-                    <styles.InputDiv>
-                        <styles.InputLabel>
-                            CyberGrants Access Code
-                        </styles.InputLabel>
-                        <TextInput value={''} />
-                    </styles.InputDiv>
-                    <styles.InputDiv>
-                        <styles.InputLabel>
-                            Additional Comments
-                        </styles.InputLabel>
-                        <TextInput value={''} />
-                    </styles.InputDiv>
-                </styles.TextFieldsDiv>
-            </Card>
-        </ThemeProvider>
-    </styles.Container>
-);
+const AddInfoCard = () => {
+    const [accessCode, setAccessCode] = useState('');
+    const [comment, setComment] = useState('');
+    return (
+        <styles.Container>
+            <ThemeProvider>
+                <Card titleHidden>
+                    <styles.HeaderDiv>
+                        <styles.HeadingDiv>
+                            <Headline fontSize={22} weight={'bold'}>
+                                <styles.Coloring>
+                                    Preferred Organization Information
+                                </styles.Coloring>
+                            </Headline>
+                        </styles.HeadingDiv>
+                        <styles.HeadingDiv>
+                            <SubHeading fontSize={18} weight={'normal'}>
+                                Tell us how you would prefer your organization to be listed in our system. We will use information from the Internal Revenue Service by default. If your organization’s name and/or address is different from what is listed with the IRS, please also provide a brief explanation as to why in the note field below.
+                            </SubHeading>
+                        </styles.HeadingDiv>
+                    </styles.HeaderDiv>
+                    <styles.TextFieldsDiv>
+                        <styles.InputDiv>
+                            <styles.InputLabel>
+                                Your Current Title/Role at this Organization
+                            </styles.InputLabel>
+                            <TextInput value={'Admin'} />
+                        </styles.InputDiv>
+                        <styles.InputDiv>
+                            <styles.InputLabel>
+                                CyberGrants Access Code
+                            </styles.InputLabel>
+                            <TextInput value={accessCode} onChange={(e => setAccessCode(e.target.value))} />
+                        </styles.InputDiv>
+                        <styles.InputDiv>
+                            <styles.InputLabel>
+                                Additional Comments
+                            </styles.InputLabel>
+                            <TextInput value={comment} onChange={(e => setComment(e.target.value))} />
+                        </styles.InputDiv>
+                    </styles.TextFieldsDiv>
+                </Card>
+            </ThemeProvider>
+        </styles.Container>
+    );
+};
 
 export default AddInfoCard;
